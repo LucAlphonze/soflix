7f692c2260f1175d4990302bcaf6c0601d1717b6
sofiarocchietti <you@example.com>
2021-05-25 17:18:16 -0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import './App.css'
+import { Route } from 'react-router-dom'
 import Row from './components/Row'
 import requests from './requests.js'
+import Banner from './components/Banner'
+import Presentation from './components/Presentation'
+import About from './components/About'
 
 function App () {
   return (
     <div className='app'>
-      <h1>Hola perri</h1>
-      <Row 
-      title='NETFLIX ORIGINALS' 
-      fetchUrl={requests.fetchNetflixOriginals} 
-      isLargeRow 
-      />
-      <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
-      <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
-      <Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
-      <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
-      <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
-      <Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies} />
-      <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
+      <Banner />
+      <Route exact path='/' component={Presentation} />
+      <Route exact path= '/about' component= {About} /> 
+      <Route exact path='/home' render={() => <Row title='NETFLIX ORIGINALS' fetchUrl={requests.fetchNetflixOriginals} isLargeRow />} />
+      <Route exact path='/home' render={() => <Row title='Trending Now' fetchUrl={requests.fetchTrending} />} />
+      <Route exact path='/home' render={() => <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />} />
+      <Route exact path='/home' render={() => <Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />} />
+      <Route exact path='/home' render={() => <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />} />
+      <Route exact path='/home' render={() => <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />} />
+      <Route exact path='/home' render={() => <Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies} />} />
+      <Route exact path='/home' render={() => <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />} />
     </div>
   )
 }
---
+++ src/components/Presentation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Presentation() {
+    return (
+        <div>
+            <h1>QUE ONDA PERRI</h1>
+        </div>
+    )
+}
+
+export default Presentation;
