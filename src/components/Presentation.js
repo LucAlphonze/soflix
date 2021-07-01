6b982ea31c52a5472112cd5ff52ec18e55c0b8a2
sofiarocchietti <you@example.com>
2021-07-01 13:28:16 -0300

    last-one

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -1,24 +1,14 @@
 import React from 'react'
 import './Presentation.css'
-import p from '../img/presentation..png'
 import logo from '../img/logo.png'
 import { Link } from 'react-router-dom';
 import { Fade } from 'react-awesome-reveal';
 
 function Presentation() {
-
-   /*  const handlerHome = () => {
-        return <NavLink exact to="/"></NavLink>
-    } */
     return (
         <div className='background'>
             <Fade cascade={true}> 
             <div className='p_container'>
-          {/*   <img 
-                className='presentation_image'
-                src={p}
-                alt='Hello there!'
-                /> */}
                 <h2 className='welcome'> WELCOME TO </h2>
                 <img className='logo_p' src={logo} alt='SOFLIX' />
                 <button className='button_home'><Link to='/home' className='link_text'>  HOME  </Link></button>
